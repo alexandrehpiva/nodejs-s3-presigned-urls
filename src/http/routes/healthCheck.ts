6aece976c7538ab6dcd022ca64a09b83733d93cd
Alexandre Piva <alexandrehpiva@gmail.com>
2021-09-16 11:29:05 -0300

    Add console log message in healthCheck

--- src/http/routes/healthCheck.ts
+++ src/http/routes/healthCheck.ts
@@ -3,6 +3,7 @@ import express from 'express';
 const healthCheck = express.Router();
 
 healthCheck.get('/', (_req, res) => {
+  console.info('health checked!');
   res.status(200);
   res.send('OK');
 });
